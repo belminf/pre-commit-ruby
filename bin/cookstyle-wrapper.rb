4562008ac3410a7a1c1dc02521c03de5bea03e6c
Belmin Fernandez <belmin.fernandez@crowdstrike.com>
2019-03-18 23:45:31 -0400

    Add cookstyle hook

---
+++ bin/cookstyle-wrapper.rb
@@ -0,0 +1,13 @@
+#!/usr/bin/env ruby
+# frozen_string_literal: true
+
+require 'fileutils'
+
+# Exit early if there are no paths to lint
+exit(true) if ARGV.empty?
+
+# Install foodcritic and drop args that are paths
+system('bundle install &>/dev/null') || exit(false)
+
+# Remove duplicates and run rspec in alphabetical order against directories
+system("bundle exec cookstyle #{ARGV.join(' ')}") || exit(false)
